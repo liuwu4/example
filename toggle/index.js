c013e26a5295ac0e6332fc01f36fbf374b0e673f
liuwu4 <liuwu4@yeah.net>
2021-01-01 22:42:25 +0800

    feat: 实现元素在某一个范围内拖动

--- toggle/index.js
+++ toggle/index.js
@@ -1,27 +1,39 @@
 var startLeft = 0;
 var startTop = 0;
+var cursorLeft = 0;
+var cursorTop = 0;
 function main() {
   var drag = document.getElementById("drag");
   var list = document.createElement("div");
   list.setAttribute("class", "list");
-  list.onmousedown = function () {
+  list.onmousedown = function (event) {
     var position = list.getBoundingClientRect();
     startLeft = position.left;
     startTop = position.top;
+    cursorLeft = event.clientX;
+    cursorTop = event.clientY;
+    list.style.cursor = 'move';
   };
-
-  drag.onmousemove = function (event) {
-    const { x, y, offsetX, offsetY, pageX, pageY, clientX, clientY } = event;
-
+  list.onmousemove = function (event) {
+    const { clientX, clientY } = event;
     if (startLeft && startTop) {
+      var positionLeft = startLeft + (clientX - cursorLeft);
+      var positionTop = startTop + (clientY - cursorTop);
+      if (drag.offsetLeft > positionLeft || !(positionLeft + list.getBoundingClientRect().width < drag.offsetLeft + drag.getBoundingClientRect().width)) {
+        positionLeft = startLeft;
+        positionTop = startTop;
+        startLeft = 0;
+        startTop = 0;
+      }
+      if (drag.offsetTop > positionTop || (list.getBoundingClientRect().height + positionTop > drag.getBoundingClientRect().height + drag.offsetTop)) {
+        positionLeft = startLeft;
+        positionTop = startTop;
+        startLeft = 0;
+        startTop = 0;
+      }
       list.style.position = "absolute";
-      console.log(startLeft, startTop, {
-        x,
-        y,
-        offsetX,
-        offsetY,
-      });
-      console.log("skjfsak");
+      list.style.left = positionLeft + "px";
+      list.style.top = positionTop + "px";
     }
   };
   list.onmouseout = function () {
