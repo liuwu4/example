48fc4eff32a210c6643098de4ae7a4dbebba9d9a
liuwu4 <liuwu4@yeah.net>
2021-01-02 20:13:50 +0800

    feat: 循环生成多个节点, copy目标节点移动

--- toggle/index.js
+++ toggle/index.js
@@ -4,50 +4,92 @@ var cursorLeft = 0;
 var cursorTop = 0;
 function main() {
   var drag = document.getElementById("drag");
-  var list = document.createElement("div");
-  list.setAttribute("class", "list");
-  list.setAttribute('onselectstart', "return false;");
-  list.onmousedown = function (event) {
-    var position = list.getBoundingClientRect();
-    startLeft = position.left;
-    startTop = position.top;
-    cursorLeft = event.clientX;
-    cursorTop = event.clientY;
-  };
-  list.onmousemove = function (event) {
+  generateNode(drag);
+
+}
+
+/**
+ * 生成多个不同位置的节点
+ * @param {*} parentNode 父节点 
+ */
+function generateNode(parentNode) {
+  for (var i = 0; i < 2; i++) {
+    var div = document.createElement('div');
+    div.innerText = i;
+    div.setAttribute('class', 'list list' + i);
+    div.setAttribute('onselectstart', "return false;");
+    div.onmousedown = (function (div) {
+      return function (event) {
+        var position = div.getBoundingClientRect();
+        startLeft = position.left;
+        startTop = position.top;
+        cursorLeft = event.clientX;
+        cursorTop = event.clientY;
+        var clone = div.cloneNode(true);
+        clone.innerText = div.innerText;
+        clone.style.position = "absolute";
+        clone.style.left = startLeft + "px";
+        clone.style.top = startTop + "px";
+        drag.appendChild(clone);
+        moveDOM(clone, div);
+      }
+    })(div);
+    parentNode.appendChild(div);
+  }
+}
+
+/**
+ * copy可移动节点
+ * @param {htmlDom} clone 
+ */
+function moveDOM(clone, soruceNode) {
+  clone.onmousemove = function (event) {
+    soruceNode.setAttribute('class', 'source');
     if (startLeft && startTop) {
-      list.style.cursor = 'move';
+      clone.style.cursor = 'move';
       var positionLeft = startLeft + (event.clientX - cursorLeft);
       var positionTop = startTop + (event.clientY - cursorTop);
-      if (drag.offsetLeft > positionLeft || !(positionLeft + list.getBoundingClientRect().width < drag.offsetLeft + drag.getBoundingClientRect().width)) {
+      if (drag.offsetLeft > positionLeft
+        || !(positionLeft + clone.getBoundingClientRect().width < drag.offsetLeft + drag.getBoundingClientRect().width)
+        || drag.offsetTop > positionTop
+        || (clone.getBoundingClientRect().height + positionTop > drag.getBoundingClientRect().height + drag.offsetTop)
+      ) {
         positionLeft = startLeft;
         positionTop = startTop;
         startLeft = 0;
         startTop = 0;
+        soruceNode.setAttribute('class', clone.getAttribute('class'));
+        clone.remove();
       }
-      if (drag.offsetTop > positionTop || (list.getBoundingClientRect().height + positionTop > drag.getBoundingClientRect().height + drag.offsetTop)) {
-        positionLeft = startLeft;
-        positionTop = startTop;
-        startLeft = 0;
-        startTop = 0;
-      }
-      list.style.position = "absolute";
-      list.style.left = positionLeft + "px";
-      list.style.top = positionTop + "px";
+      clone.style.left = positionLeft + "px";
+      clone.style.top = positionTop + "px";
     }
   };
-  list.onmouseout = function () {
-    list.style.cursor = 'default';
+  clone.onmouseout = function (event) {
+    clone.style.cursor = 'default';
     startLeft = 0;
     startTop = 0;
+    soruceNode.setAttribute('class', clone.getAttribute('class'));
+    clone.remove();
   };
-  list.onmouseup = function () {
-    list.style.cursor = 'default';
+  clone.onmouseup = function (event) {
+    exchange(clone, event);
+    clone.style.cursor = 'default';
     startLeft = 0;
     startTop = 0;
+    soruceNode.setAttribute('class', clone.getAttribute('class'));
+    clone.remove();
   };
-  list.innerText = 1234;
-  drag.appendChild(list);
+}
+
+/**
+ * 交换
+ * @param {*} start 开始时的target
+ * @param {*} event 结束时事件的event
+ */
+function exchange(start, event) {
+  var postion = start.getBoundingClientRect();
+  // console.log(start.offsetLeft, event.clientX);
 }
 
 main();
