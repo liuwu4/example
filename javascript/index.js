a998f5a5a1aa14300a9194e419bb822ff6e22145
yj <liuwu4@yeah.net>
2021-06-23 21:39:49 +0800

    docs: 记录

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,21 @@
 /*
  * @Author: yj
  * @Date: 2021-04-26 11:45:53
- * @LastEditTime: 2021-06-22 13:56:21
+ * @LastEditTime: 2021-06-23 21:01:21
  * @Description: js 测试
  */
 
-const shape = {
-  radius: 10,
-  fn: (function () {
-    return () => {
-      console.log(this.radius);
-    };
-  })(),
-  path: () => {
-    console.log(Math.PI, this);
-  },
-};
+function* run() {
+  yield 1;
+  yield 2;
+}
 
-// const a = {};
-// const b = { key: "123" };
-// const c = { key: "mm" };
-// a[b] = "132";
-// console.log(a[b], {} + "b");
-function run() {}
-const Run = new run();
+async function testAsync() {
+  await 1;
+}
 
-console.log("----", Run, Run.__proto__ == run.prototype);
-console.log("function:", run.prototype);
+const test = run();
+
+console.log(test.next(), test.next());
+
+console.log(testAsync());
